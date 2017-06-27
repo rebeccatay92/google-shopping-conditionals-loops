4d4cf4291cdc63ab824e122260e9ad28ce23c077
Nicholas Lee <niconal902@gmail.com>
2017-06-27 08:20:34 +0800

    added jocelyn's changes

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -2,25 +2,29 @@ var data = require('../products.json')
 
 // Write your solutions below
 
-// console.log(data.items);
 var items = data.items
 
+// Q1)
 // var itemCount = 0
-
-// 1 Go through the items and find all results that have kind of shopping#product. Print the count of these results. Where else is this count information stored in the search results?
-
 // for (var i = 0; i < items.length; i++) {
 //   var eachItem = items[i]
 //   itemCount += 1
 //   console.log(eachItem.kind)
 // }
 
-// 2 Print the title all items with a backorder availability in inventories.
+// Q2)
+// for (var j = 0; j < items.length; j++) {
+//   var eachItem = items[j]
+//   var inventories = eachItem.product.inventories[0]
+//   if (inventories.availability === 'backorder') {
+//     console.log(eachItem.product.brand)
+//   }
+// }
 
-for (var j = 0; j < items.length; j++) {
-  var eachItem = items[j]
-  var inventories = eachItem.product.inventories[0]
-  if (inventories.availability === 'backorder') {
-    console.log(eachItem.product.brand)
-  }
-}
+// Q3
+// for (var i = 0; i < data.items.length; i++) {
+//   var itemsProduct = data.items[i].product
+//   if (itemsProduct.images.length > 1) {
+//     console.log(itemsProduct.title) // ---> Nikon D800 ... Canon EOS
+//   }
+// }
